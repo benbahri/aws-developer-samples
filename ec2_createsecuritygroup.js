028e6ed8fc504f1caec6da5451cd05d036ec338c
ahhosni <ahmedhosni.contact@gmail.com>
2023-01-31 16:25:35 +0100

    add security group

--- ec2_createsecuritygroup.js
+++ ec2_createsecuritygroup.js
@@ -1,5 +1,5 @@
 import { ec2Client } from "./libs/ec2Client.js"
-import { CreateSecurityGroupCommand, DescribeVpcsCommand, AuthorizeSecurityGroupIngressCommand } from "@aws-sdk/client-ec2";
+import { CreateSecurityGroupCommand, DescribeVpcsCommand, AuthorizeSecurityGroupIngressCommand, AttachNetworkInterfaceCommand } from "@aws-sdk/client-ec2";
 
 const params = { KeyName: "Ahmed_keypair_dev" };
 
@@ -10,7 +10,36 @@ const run = async () => {
     const data = await ec2Client.send(new DescribeVpcsCommand(params));
     vpc = data.Vpcs[0].VpcId;
     console.log("VPC details: ", vpc);
-    return data;
+   } catch (error) {
+    console.log("Error", error);
+   }
+
+   try {
+    const paramSecurityGroup = { 
+        Description: "SG created with nodejs SDK ",
+        GroupName: "NODEJS_SDK_Security_Group",
+        VpcId: vpc,
+    };
+    const datasg = await ec2Client.send(new CreateSecurityGroupCommand(paramSecurityGroup));
+    const groupId = datasg.GroupId;
+    console.log("Success", groupId);
+
+    const ingressParams = { 
+        GroupId: groupId,
+        IpPermissions:[
+            { 
+                IpProtocol: "tcp", FromPort: 80, ToPort: 80, IpRanges: [{ CidrIp: "0.0.0.0/0"}],
+            } ,
+            { 
+                IpProtocol: "tcp", FromPort: 22, ToPort: 22, IpRanges: [{ CidrIp: "0.0.0.0/0"}],
+            }
+        ]
+    };
+ 
+   const data = await ec2Client.send(new AuthorizeSecurityGroupIngressCommand(ingressParams));
+   console.log(data);
+   return data;
+
    } catch (error) {
     console.log("Error", error);
    }
