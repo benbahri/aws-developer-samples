181e4eee71c95a8c5b56906ba1910706c4417434
ahhosni <ahmedhosni.contact@gmail.com>
2023-01-31 16:47:43 +0100

    Dynamically fetch instance Idd

--- ec2_reboot.js
+++ ec2_reboot.js
@@ -1,12 +1,18 @@
 import { ec2Client } from "./libs/ec2Client.js"
-import { RebootInstancesCommand} from "@aws-sdk/client-ec2";
+import { RebootInstancesCommand, DescribeInstancesCommand} from "@aws-sdk/client-ec2";
 
-const params = { InstanceIds: ["i-0cb0e0b37f060248f"]}
 
 const run = async () => {
     try {
+
+       const ec2data = await ec2Client.send(new DescribeInstancesCommand({}));
+       const instance = ec2data.Reservations[0].Instances[0].InstanceId;
+       //console.log("sucess", ec2data.Reservations[0].Instances[0].InstanceId);
+       const params = { InstanceIds: [instance]}
+
         const data = await ec2Client.send(new RebootInstancesCommand(params));
         console.log("sucess", data);
+        
         return data;
     } catch (error) {
         console.log("Error", error);
