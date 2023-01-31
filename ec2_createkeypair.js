944e5791d3608387fa25bbc9da74395b62221a65
ahhosni <ahmedhosni.contact@gmail.com>
2023-01-31 14:39:32 +0100

    Create a new keypair

--- ec2_createkeypair.js
+++ ec2_createkeypair.js
@@ -1,2 +1,17 @@
-import { ec2Client } from "./libs/ec2Client"
-import { ec2Client } from "@aws-sdk/client-ec2"
+import { ec2Client } from "./libs/ec2Client.js"
+import { CreateKeyPairCommand } from "@aws-sdk/client-ec2";
+
+const params = { KeyName: "Ahmed_keypair_dev" }
+
+const run = async() => { 
+    try {
+        let keypairCommand = new CreateKeyPairCommand(params);
+        const data = await ec2Client.send(keypairCommand);
+        console.log("Success", data);
+        return data;
+    } catch (error) {
+        console.log("Error", err);
+    }
+ }
+
+ run();
