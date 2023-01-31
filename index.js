df5ba8d5f29b7e5c543cbb662d05bbce60d2b34a
ahhosni <ahmedhosni.contact@gmail.com>
2023-01-31 12:18:40 +0100

    list S3bukets

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-import { ListObjectsCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import { ListBucketsCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "./libs/sampleClient.js";
 
-export const params = {
-    Bucket: "datascientest-ahmed-sdkdemo", 
-    Key: "demo.txt", 
-    Body: "Hello Datascientext!", 
-  };
-
-  export const bucket =  {
-    Bucket: "datascientest-ahmed-sdkdemo"
-  };
 
 export const run = async () => { 
     try {
-        const data = await s3Client.send(new PutObjectCommand(params));
-        console.log(
-            "Successfully created " +
-            params.Key +
-            " and uploaded it to " +
-            params.Bucket +
-            "/" +
-            params.Key
+        const data = await s3Client.send(new ListBucketsCommand({}));
+        data.Buckets.forEach(  
+            element => console.log( element.CreationDate.toUTCString() + " " + element.Name ) 
         );
-
-        const result = await s3Client.send(new ListObjectsCommand(bucket));
-        console.log("Debug: ", result)
-
         return data;
     } catch (err) {
         console.log("Error", err);
