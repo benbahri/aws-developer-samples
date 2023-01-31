d6e4dd0658b9f5c740c95db934e104c83d24c4e6
ahhosni <ahmedhosni.contact@gmail.com>
2023-01-31 11:46:56 +0100

    create and list s3 object

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-import { CreateBucketCommand } from "@aws-sdk/client-s3";
+import { ListObjectsCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "./libs/sampleClient.js";
 
-export const bucketParams = { Bucket: "datascientest-ahmed-sdkdemo" };
+export const params = {
+    Bucket: "datascientest-ahmed-sdkdemo", 
+    Key: "demo.txt", 
+    Body: "Hello Datascientext!", 
+  };
+
+  export const bucket =  {
+    Bucket: "datascientest-ahmed-sdkdemo"
+  };
 
 export const run = async () => { 
     try {
-        const data = await s3Client.send(new CreateBucketCommand(bucketParams));
-        console.log("Success", data);
+        const data = await s3Client.send(new PutObjectCommand(params));
+        console.log(
+            "Successfully created " +
+            params.Key +
+            " and uploaded it to " +
+            params.Bucket +
+            "/" +
+            params.Key
+        );
+
+        const result = await s3Client.send(new ListObjectsCommand(bucket));
+        console.log("Debug: ", result)
+
         return data;
     } catch (err) {
         console.log("Error", err);
