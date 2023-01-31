1515840995600d73d9c94afa338e134c78521ff4
ahhosni <ahmedhosni.contact@gmail.com>
2023-01-31 15:15:00 +0100

    Add tagging to the VM creation

--- ec2_createinstance.js
+++ ec2_createinstance.js
@@ -1,15 +1,8 @@
 import { ec2Client } from "./libs/ec2Client.js"
-import { RunInstancesCommand } from "@aws-sdk/client-ec2";
-
-// KEY_PAIR-NAME: Ahmed_keypair_dev
-//Image_ID: ami-0aa7d40eeae50c9a9
+import { RunInstancesCommand, CreateTagsCommand} from "@aws-sdk/client-ec2";
 
 const params = { 
-    ImageId: "ami-0aa7d40eeae50c9a9",
-    InstanceType: "t2.micro",
-    KeyName: "Ahmed_keypair_dev",
-    MinCount: 1,
-    MaxCount: 1,
+    ImageId: "ami-0aa7d40eeae50c9a9", InstanceType: "t2.micro", KeyName: "Ahmed_keypair_dev", MinCount: 1, MaxCount: 1,
  }
 
  const run = async () => { 
@@ -18,6 +11,18 @@ const params = {
         const data = await ec2Client.send(ec2command);
         const instanceId = data.Instances[0].InstanceId;
         console.log("created instance: ", instanceId);
+
+        const tagParams = { 
+            Resources: [instanceId],
+            Tags: [{ Key: "Name", Value: "SDK-powered-VM", },],
+         };
+         try {
+            await ec2Client.send(new CreateTagsCommand(tagParams));
+            console.log("Instance tagged: ", instanceId )
+         } catch (error) {
+            
+         }
+
     } catch (error) {
         console.log("Error", err)
     }
