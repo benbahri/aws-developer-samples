e513e0cd9b76ab45dd9ecb6e6da2cf0f694c1da0
ahhosni <ahmedhosni.contact@gmail.com>
2023-01-31 14:29:37 +0100

    Add lib for EC2 managment

---
+++ ec2_createkeypair.js
@@ -0,0 +1,2 @@
+import { ec2Client } from "./libs/ec2Client"
+import { ec2Client } from "@aws-sdk/client-ec2"
--- libs/ec2Client.js
+++ libs/ec2Client.js
@@ -1,4 +1,4 @@
-const { EC2Client } = require("@aws-sdk/client-s3");
+import { EC2Client } from  "@aws-sdk/client-ec2";
 
 const REGION = "us-east-1";
 const ec2Client = new EC2Client({ REGION });
